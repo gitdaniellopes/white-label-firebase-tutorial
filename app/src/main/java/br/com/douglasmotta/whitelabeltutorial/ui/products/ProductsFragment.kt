89dceab3830ade2d4b441d09ac8fefb5586a4d13
Daniel Lopes <danieljfsantos@gmail.com>
2022-02-03 16:03:50 -0300

    Implementando ViewModel da Listagem de Produtos

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/ProductsFragment.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/ProductsFragment.kt
@@ -5,10 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import br.com.douglasmotta.whitelabeltutorial.databinding.FragmentProductsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProductsFragment : Fragment() {
 
+    private val viewModel: ProductsViewModel by viewModels()
+    private val productsAdapter by lazy { ProductsAdapter() }
     private var _binding: FragmentProductsBinding? = null
     private val binding get() = _binding!!
 
@@ -19,5 +24,31 @@ class ProductsFragment : Fragment() {
     ): View {
         _binding = FragmentProductsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecycleView()
+        observers()
+        viewModel.getProducts()
+    }
+
+    private fun setupRecycleView() {
+        binding.recyclerProducts.run {
+            setHasFixedSize(true)
+            adapter = productsAdapter
+        }
+    }
+
+    private fun observers() {
+        viewModel.productsData.observe(viewLifecycleOwner) { products ->
+            productsAdapter.submitList(products)
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
